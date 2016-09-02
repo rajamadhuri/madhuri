d33ed42b3fefdec8063125846358873ab6cf351a
rajamadhuri <madhurimurugesapandian@gmail.com>
2016-09-02 21:53:58 +0530

    Create Mod.java

---
+++ Mod.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public clas Mod
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number:");
+n=sc.nextInt();
+if((n/2)*2==n)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
