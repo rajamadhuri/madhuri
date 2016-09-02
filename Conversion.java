119c06e08eb4d28ad644f55215488f22ccc6a6e1
rajamadhuri <madhurimurugesapandian@gmail.com>
2016-09-02 20:42:15 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Conversion
+{
+public static void main(String args[])
+{
+int tinc,tinf;
+System.out.println("enter the temperature in celcius:");
+Scanner sc=new Scanner(System.in);
+tinc=sc.nextInt();
+tinf=(tinc*(9/5))+32;
+System.out.println("temperature in farenheit:"+tinf);
+}
+}
